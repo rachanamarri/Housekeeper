654dc6476fd7042c5fe4c5d40f9f47d80aed2a9c
Nitheesha <42808133+nitheeshareddy20@users.noreply.github.com>
2022-01-29 21:39:29 -0500

    adding backend

---
+++ app_backend/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	uuid "github.com/satori/go.uuid"
+)
+
+type Seeker struct {
+	Id       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+}
+
+func main() {
+
+	//creating Database using gorm(an ORM which simplifies the mapping and persistance of the models to the database)
+	db, err := gorm.Open("sqlite3", "./dbase.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Seeker{})
+
+	//creating variable using gin Web Framework to handle routing and serving HTTP requests
+	r := gin.Default()
+
+	r.GET("/", h)
+	r.Run(":8080")
+}
+
+func h(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "This is the main page of our HouseKeeper Website. Here only two login/register buttons will be displayed saperately for user and service provider. This selection will redirect them to the next pages/routes....."})
+}
