a6c02929c80cae9bcae83f531551b8890fce76db
Nitheesha <42808133+nitheeshareddy20@users.noreply.github.com>
2022-02-02 10:55:21 -0500

    seeker/provider registration

--- app_backend/main.go
+++ app_backend/main.go
@@ -1,29 +1,33 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	uuid "github.com/satori/go.uuid"
 )
 
 type Seeker struct {
-	Id       uuid.UUID `json:"id"`
-	Username string    `json:"username"`
-	Password string    `json:"password"`
+	Name     string `json:"Name"`
+	Email    string `json:"Email"`
+	Password string `json:"password"`
+	Address  string `json:"address"`
 }
 
 type Provider struct {
-	Id       uuid.UUID `json:"id"`
-	Username string    `json:"username"`
-	Password string    `json:"password"`
-	Service  string    `json:"service"`
+	ServiceName string `json:"serviceName"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
 }
+
+var db *gorm.DB
+var err error
 
 func main() {
 
 	//creating Database using gorm(an ORM which simplifies the mapping and persistance of the models to the database)
-	db, err := gorm.Open("sqlite3", "./dbase.db")
+	db, err = gorm.Open("sqlite3", "./dbase.db")
 	if err != nil {
 		panic(err)
 	}
@@ -36,15 +40,38 @@ func main() {
 	//r :=gin.Default() does not work, it gives a huge runtime error
 	r := gin.New()
 
+	//Routers
 	r.GET("/", home)
+	r.POST("/seeker_registration", create_seeker)
+	r.POST("/provider_registration", create_provider)
+	r.POST("/seeker_login", nil)
+	r.POST("/provider_login", nil)
+	r.GET("/seeker_home", nil)
+	r.GET("/provider_home", nil)
 
 	r.Run(":8080")
 }
 
-func crud_func(c *gin.Context) {
+func create_seeker(c *gin.Context) {
+
+	var seeker Seeker
+	c.BindJSON(&seeker)
+	db.Create(&seeker)
+	c.JSON(200, seeker)
+	fmt.Println("successfully added an entry into seeker DB !")
+
+}
+
+func create_provider(c *gin.Context) {
+
+	var provider Provider
+	c.BindJSON(&provider)
+	db.Create(&provider)
+	c.JSON(200, provider)
+	fmt.Println("successfully added an entry into provider DB !")
 
 }
 
 func home(c *gin.Context) {
-	c.JSON(200, gin.H{"message": "This is the main page of our HouseKeeper Website. Here only two login/register buttons will be displayed saperately for user and service provider. This selection will redirect them to the next pages/routes....."})
+	c.JSON(200, gin.H{"message": "Home Page"})
 }
