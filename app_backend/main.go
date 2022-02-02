22980fdb6ffe212ece13f6b531bcf6d0636842f6
Nitheesha <42808133+nitheeshareddy20@users.noreply.github.com>
2022-02-01 19:39:07 -0500

    runnning server without error

--- app_backend/main.go
+++ app_backend/main.go
@@ -13,6 +13,13 @@ type Seeker struct {
 	Password string    `json:"password"`
 }
 
+type Provider struct {
+	Id       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Service  string    `json:"service"`
+}
+
 func main() {
 
 	//creating Database using gorm(an ORM which simplifies the mapping and persistance of the models to the database)
@@ -23,14 +30,21 @@ func main() {
 	defer db.Close()
 
 	db.AutoMigrate(&Seeker{})
+	db.AutoMigrate(&Provider{})
 
 	//creating variable using gin Web Framework to handle routing and serving HTTP requests
+	//r :=gin.Default() does not work, it gives a huge runtime error
 	r := gin.New()
 
-	r.GET("/", h)
+	r.GET("/", home)
+
 	r.Run(":8080")
 }
 
-func h(c *gin.Context) {
+func crud_func(c *gin.Context) {
+
+}
+
+func home(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "This is the main page of our HouseKeeper Website. Here only two login/register buttons will be displayed saperately for user and service provider. This selection will redirect them to the next pages/routes....."})
 }
