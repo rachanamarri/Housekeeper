ab3a1cb118a3fb04e20a42fe14464c631ce3ee8a
Nitheesha <42808133+nitheeshareddy20@users.noreply.github.com>
2022-02-02 19:39:21 -0500

    added login authorisation functionality without session

--- app_backend/main.go
+++ app_backend/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -21,6 +23,11 @@ type Provider struct {
 	Password    string `json:"password"`
 }
 
+type Login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 var db *gorm.DB
 var err error
 
@@ -35,6 +42,7 @@ func main() {
 
 	db.AutoMigrate(&Seeker{})
 	db.AutoMigrate(&Provider{})
+	db.AutoMigrate(&Login{})
 
 	//creating variable using gin Web Framework to handle routing and serving HTTP requests
 	//r :=gin.Default() does not work, it gives a huge runtime error
@@ -44,10 +52,11 @@ func main() {
 	r.GET("/", home)
 	r.POST("/seeker_registration", create_seeker)
 	r.POST("/provider_registration", create_provider)
-	r.POST("/seeker_login", nil)
-	r.POST("/provider_login", nil)
+	r.POST("/seeker_login", login_auth)
+	r.POST("/provider_login", login_auth)
 	r.GET("/seeker_home", nil)
 	r.GET("/provider_home", nil)
+	r.GET("/service/:id", nil)
 
 	r.Run(":8080")
 }
@@ -55,9 +64,17 @@ func main() {
 func create_seeker(c *gin.Context) {
 
 	var seeker Seeker
+	var login Login
+
 	c.BindJSON(&seeker)
+
+	login.Email = seeker.Email
+	login.Password = seeker.Password
+
 	db.Create(&seeker)
-	c.JSON(200, seeker)
+	db.Create(&login)
+
+	c.JSON(http.StatusOK, gin.H{"message": "working !!"})
 	fmt.Println("successfully added an entry into seeker DB !")
 
 }
@@ -65,10 +82,43 @@ func create_seeker(c *gin.Context) {
 func create_provider(c *gin.Context) {
 
 	var provider Provider
+	var login Login
+
 	c.BindJSON(&provider)
+
+	login.Email = provider.Email
+	login.Password = provider.Password
+
 	db.Create(&provider)
+	db.Create(&login)
+
 	c.JSON(200, provider)
 	fmt.Println("successfully added an entry into provider DB !")
+
+}
+
+func login_auth(c *gin.Context) {
+
+	var auth Login
+	var storedAuth Login
+
+	c.BindJSON(&auth)
+
+	err := db.Where("Email = ?", auth.Email).First(&storedAuth).Error
+	if err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		match := strings.Compare(auth.Password, storedAuth.Password)
+		if match == 0 {
+			fmt.Println("match")
+			c.JSON(200, gin.H{"message": "login successful!"})
+		} else {
+			fmt.Println("No match")
+			c.JSON(401, gin.H{"message": "Login Failed!"})
+		}
+
+	}
 
 }
 
