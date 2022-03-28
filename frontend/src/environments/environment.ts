7d7e7d38d1b6f16fb41ab26575514cdc68672018
Yasaswini_Valisetty <yasaswinivalisetty@gmail.com>
2022-03-28 00:07:38 -0400

    Export env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
+// The following file can be replaced while build with the use of the array `fileReplacements`.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false
 };
