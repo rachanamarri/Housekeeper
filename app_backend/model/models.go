79c90d13544c3f9eb199ec030a46e8ace0640368
Yasaswini_Valisetty <yasaswinivalisetty@gmail.com>
2022-04-20 12:09:35 -0500

    pkg model

--- app_backend/model/models.go
+++ app_backend/model/models.go
@@ -1,40 +1,105 @@
 package model
 
+/* Importing git repository*/
+
 import "github.com/markphelps/flipt/storage/sql/common"
 
 type Seeker struct {
+
 	Name     string `json:"Name"`
 	Email    string `json:"Email"`
 	Password string `json:"password"`
 	Address  string `json:"address"`
+
 }
 
 type Login struct {
+
 	Email    string `json:"email"`
 	Password string `json:"password"`
+
 }
 
 type ServiceAndProvider struct {
+
 	ServiceId          int64  `json:"ServiceId"`
 	ServiceName        string `json:"ServiceName"`
 	ProviderEmail      string `json:"ProviderEmail"`
 	ProviderPassword   string `json:"ProviderPassword"`
 	ServicePrice       int64  `json:"ServicePrice"`
 	ServiceDescription string `json:"ServiceDescription"`
+
 }
 
 type Booking struct {
 	ServiceId   int64  `json:"ServiceId"`
 	SeekerName  string `json:"SeekerName"`
 	SeekerEmail string `json:"SeekerEmail"`
+
 }
 
 type Store struct {
+
 	*common.Store
 }
 
 type Ratings struct {
+
 	ServiceID     int64  `json:"ServiceId"`
 	ProviderEmail string `json:"ProviderEmail"`
 	Rating        int64  `json:"rating"`
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
